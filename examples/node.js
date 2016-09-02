ea466e1e2515b6aeaa8d6cca868b8db641e5308d
erickeno <erickeno12@gmail.com>
2016-09-02 13:03:13 -0400

    include tests - included test for function params

--- examples/node.js
+++ examples/node.js
@@ -1,3 +1,1 @@
-import { add } from '../lib';
 
-console.log('2 + 2', '=', add(2, 2));
--- test/index.test.js
+++ test/index.test.js
@@ -2,11 +2,13 @@ import { expect } from 'chai';
 import { excelToCSV } from '../src'
 
 describe('excelToCSV', () => {
-  it('throw error if inputFile is missing', () => {
-    expect(()=> excelToCSV()).to.throw('inputFile is required')
-  });
-  it('should not throw error if inputFile is passed', () => {
-    const inputfile = 'path/to/excel/file.xlsx';
-    expect(()=> excelToCSV(inputfile)).to.not.throw('inputFile is required');
-  })
+	it('throw error if inputFile is missing', () => {
+		expect(() => excelToCSV()).to.throw('inputFile is required')
+	});
+
+	it('should not throw error if inputFile is passed', () => {
+
+		const inputfile = 'path/to/excel/file.xlsx';
+		expect(() => excelToCSV(inputfile)).to.not.throw('inputFile is required');
+	})
 });
