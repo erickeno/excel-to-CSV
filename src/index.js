84527ae1c72a1598fae807acc87e7d1a996a5cb8
erickeno <erickeno12@gmail.com>
2016-09-02 13:04:52 -0400

    include excelToCSV to pass failing tests - Included the excelToCSV to pass the failing tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+import XLSX from 'xlsx'
+import fs from 'fs'
+import json2csv from 'json2csv'
 
+export const excelToCSV = (inputFile) => {
+	if (!inputFile) {
+		throw new Error('inputFile is required')
+	}
+	const outputFile = inputFile.replace(/xlsx$/ig, 'csv')
+
+	const workbook = XLSX.readFile(inputFile);
+	const jsonFile = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+
+	json2csv({
+			data: jsonFile,
+			quotes: '',
+			del: '\t',
+		},
+		(err, csv) => {
+			if (err) throw err
+			fs.writeFile(outputFile, csv, err => {
+				if (err) throw err
+				console.log('file saved');
+			})
+		}
+	)
+}
